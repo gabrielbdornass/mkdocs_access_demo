5eae9a311c4484b79af9883c2f9df4745f010d63
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-11-13 18:09:22 -0300

    Improve login page redirection

--- pagesite/urls.py
+++ pagesite/urls.py
@@ -5,7 +5,7 @@ from access.views import serve_page, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("logout/", logout_view, name="logout"),
     re_path(r"^(?P<path>.*)$", serve_page),  # serve any page dynamically
 ]
